7c2e519c41fad2aa9c058ce2787c210812776707
Egor-Kozh <kozhevnikovegor55@gmail.com>
2025-03-19 12:04:01 +0600

    feat: separation-color-themes

--- myfeed-react/src/app/app.tsx
+++ myfeed-react/src/app/app.tsx
@@ -1,3 +1,16 @@
+import "./styles/global.scss";
+
 export const App = () => {
-  return <></>;
+  function toggleTheme() {
+    const html = document.documentElement;
+    const currentTheme = html.getAttribute("color-theme");
+    const newTheme = currentTheme === "light" ? "dark" : "light";
+    html.setAttribute("color-theme", newTheme);
+  }
+  return (
+    <>
+      <p>Hello world!</p>
+      <button onClick={toggleTheme}>Toggle Theme</button>
+    </>
+  );
 };
